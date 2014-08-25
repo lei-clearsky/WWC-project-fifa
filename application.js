c2e62fe2a1e3a0ba0428b84b1912a67de43b42f0
lei-clearsky <sportzhulei@gmail.com>
2014-08-25 19:03:37 -0400

    Added drag and drop functions, the country names will be updated as soon as one country item is dropped to the countainer

--- application.js
+++ application.js
@@ -10,34 +10,31 @@ var fifa = (function ($){
 		main_html : String()
 		+ '<div class="container">'
 		+	'<div class="sixteen columns">'
-		+		'<h1 class="remove-bottom fifa-home">FIFA Matches</h1>'
-		+	'</div>'
-		+	'<div class="sixteen columns">'
 		+		'<div class="matches-container">'
 		+		'<!--2nd Column-->'
 		+		'<div class="match-container">'
 		+			'<div class="round-16-container-left">'
-		+				'<div class="top">BRAZIL</div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/bra.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/chi.png" /></div>'
-		+				'<div class="bottom">CHILE</div>'
-		+			'</div>'
-		+			'<div class="round-16-container-left">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/col.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/uru.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="round-16-container-left">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/fra.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/nga.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="round-16-container-left">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/ger.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/alg.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="bottom"></div>'
+		+			'</div>'
+		+			'<div class="round-16-container-left">'
+		+				'<div class="top"></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+		'</div>'
@@ -45,16 +42,16 @@ var fifa = (function ($){
 		+		'<div class="match-container">'
 		+			'<div class="quarter-finals-container-left">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/bra.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/col.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="quarter-finals-space">'
 		+			'</div>'
 		+			'<div class="quarter-finals-container-left">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/fra.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/ger.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+		'</div>'
@@ -62,8 +59,8 @@ var fifa = (function ($){
 		+		'<div class="match-container">'
 		+			'<div class="semi-finals-container-left">'	
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/bra.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/ger.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'				
 		+			'</div>'
 		+		'</div>'
@@ -71,18 +68,18 @@ var fifa = (function ($){
 		+		'<div class="match-container">'
 		+			'<div class="final-space">'
 		+				'<h4>WINNER</h4>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/ger.png" /></div>'
+		+				'<div class="drop-to"></div>'
 		+			'</div>'
 		+			'<div class="final-container">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/ger.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/arg.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="final-container">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/bra.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/ned.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+		'</div>'
@@ -90,8 +87,8 @@ var fifa = (function ($){
 		+		'<div class="match-container">'
 		+			'<div class="semi-finals-container-right">'	
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/ned.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/arg.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'			
 		+			'</div>'
 		+		'</div>'
@@ -99,16 +96,16 @@ var fifa = (function ($){
 		+		'<div class="match-container">'
 		+			'<div class="quarter-finals-container-right">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/ned.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/crc.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="quarter-finals-space">'
 		+			'</div>'
 		+			'<div class="quarter-finals-container-right">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/arg.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/bel.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+		'</div>'
@@ -116,26 +113,26 @@ var fifa = (function ($){
 		+		'<div class="match-container">'
 		+			'<div class="round-16-container-right">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/ned.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/mex.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="round-16-container-right">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/crc.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/gre.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="round-16-container-right">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/arg.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/sui.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+			'<div class="round-16-container-right">'
 		+				'<div class="top"></div>'
-		+				'<div id="draggable1"><img src="http://img.fifa.com/images/flags/4/bel.png" /></div>'
-		+				'<div id="draggable2"><img src="http://img.fifa.com/images/flags/4/usa.png" /></div>'
+		+				'<div class="drop-to"></div>'
+		+				'<div class="drop-to"></div>'
 		+				'<div class="bottom"></div>'
 		+			'</div>'
 		+		'</div>'
@@ -145,7 +142,9 @@ var fifa = (function ($){
 	},
 	stateMap = {$container : null},
 	jqueryMap = {},
-	setJqueryMap, setCountry, initModule;
+	countriesArray = [],
+	setJqueryMap, setCountry, setCountryContainer, 
+	dragDrop, updateCountryName, initModule;
 
 	// Begin DOM method /setJqueryMap/
 	setJqueryMap = function (){
@@ -168,28 +167,80 @@ var fifa = (function ($){
 
 	// Use setCountry function to instantiate country objects
 	setCountry = function(){
-		var brazil = new Country('bra', 'brazil', '#');
-		var chile = new Country('chi', 'chile', '#');
-		var colombia = new Country('col', 'colombia', '#');
-		var uruguay = new Country('uru', 'uruguay', '#');
-		var france = new Country('fra', 'france', '#');
-		var nigeria = new Country('nga', 'nigeria', '#');
-		var germany = new Country('ger', 'germany', '#');
-		var algeria = new Country('alg', 'algeria', '#');
-		var belgium = new Country('bel', 'belgium', '#');
-		var usa = new Country('usa', 'usa', '#');
-		var argentina = new Country('arg', 'argentina', '#');
-		var switzerland = new Country('sui', 'switzerland', '#');
-		var greece = new Country('gre', 'greece', '#');
-		var costarica = new Country('crc', 'costarica', '#');
-		var mexico = new Country('mex', 'mexico', '#');
-		var netherlands = new Country('ned', 'netherland', '#');
+		var brazil = new Country('bra', 'brazil', 'http://img.fifa.com/images/flags/4/bra.png');
+		var chile = new Country('chi', 'chile', 'http://img.fifa.com/images/flags/4/chi.png');
+		var colombia = new Country('col', 'colombia', 'http://img.fifa.com/images/flags/4/col.png');
+		var uruguay = new Country('uru', 'uruguay', 'http://img.fifa.com/images/flags/4/uru.png');
+		var france = new Country('fra', 'france', 'http://img.fifa.com/images/flags/4/fra.png');
+		var nigeria = new Country('nga', 'nigeria', 'http://img.fifa.com/images/flags/4/nga.png');
+		var germany = new Country('ger', 'germany', 'http://img.fifa.com/images/flags/4/ger.png');
+		var algeria = new Country('alg', 'algeria', 'http://img.fifa.com/images/flags/4/alg.png');
+		var belgium = new Country('bel', 'belgium', 'http://img.fifa.com/images/flags/4/bel.png');
+		var usa = new Country('usa', 'usa', 'http://img.fifa.com/images/flags/4/usa.png');
+		var argentina = new Country('arg', 'argentina', 'http://img.fifa.com/images/flags/4/arg.png');
+		var switzerland = new Country('sui', 'switzerland', 'http://img.fifa.com/images/flags/4/sui.png');
+		var greece = new Country('gre', 'greece', 'http://img.fifa.com/images/flags/4/gre.png');
+		var costarica = new Country('crc', 'costarica', 'http://img.fifa.com/images/flags/4/crc.png');
+		var mexico = new Country('mex', 'mexico', 'http://img.fifa.com/images/flags/4/mex.png');
+		var netherlands = new Country('ned', 'netherland', 'http://img.fifa.com/images/flags/4/ned.png');
+
+		countriesArray.push(brazil, chile, colombia, uruguay, france, nigeria, germany, algeria, belgium, usa, argentina, switzerland, greece, costarica, mexico, netherlands);
 	};
+
+	// Add country objects to countries container
+	setCountryContainer = function(){
+		setCountry();
+		for (var i = 0; i < countriesArray.length; i++){
+			var countryContainer = $('<div class="country"></div>');
+			// Add country data to country container
+			countryContainer.data('country', countriesArray[i]);
+			countryContainer.html('<img src = "' + countriesArray[i].imgurl + '" />' + ' ' + countriesArray[i].abbreviation);
+			$('.countries-container').append(countryContainer);
+		}
+	}
+
+	// Drag and Drop functions
+	dragDrop = function(){
+		$( ".country" ).draggable({ containment: 'document', revert: true,
+			start: function(){
+				//contents = $(this).html();
+				contents = $(this).clone(true);
+				//alert(contents.data('country').fullname);
+
+			}
+		});
+
+		$(".drop-to").droppable({accept: ".country",
+			drop: function(){
+				if($(this).html() == ""){
+					//$(this).append('<div class="country">' + contents + '</div>');
+					$(this).append(contents);
+
+					var thisContainer = $(this);
+	
+					// Update country fullname after new item is dropped to the individual match container
+					if(thisContainer.index() % 2 !== 0){
+						thisContainer.parent().children('.top').html((thisContainer.children('.country').data('country').fullname).toUpperCase());
+					}else{
+						thisContainer.parent().children('.bottom').html((thisContainer.children('.country').data('country').fullname).toUpperCase());
+					}
+
+				}
+			}
+		});
+	}
+
+	// Update country fullname after new item is dropped to the individual match container
+	updateCountryName = function(){
+
+	}
 
 	// Begin public method /initModule/
 	initModule = function ( $container ){
 		stateMap.$container = $container;
 		$container.html(configMap.main_html);
+		setCountryContainer();
+		dragDrop();
 		setJqueryMap();
 	};
 	return { initModule: initModule };
